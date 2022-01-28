bb6bdb09b7e0da8e995057741c425776204f718c
tonyeko <tony.yuwono@gmail.com>
2022-01-28 21:07:39 +0700

    add typography components

---
+++ src/shared/typography.ts
@@ -0,0 +1,85 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import { up } from "styled-breakpoints";
+import { TypographyProps, typography } from "styled-system";
+
+/**
+ * Typography Styles
+ * Based on TailwindCSS
+ */
+
+const textSmallStyles = css`
+  font-size: 0.875rem /* 14px */;
+  line-height: 1.25rem /* 20px */;
+`;
+
+const textBaseStyles = css`
+  font-size: 1rem /* 16px */;
+  line-height: 1.5rem; /* 24px */
+`;
+
+const textLargeStyles = css`
+  font-size: 1.125rem /* 18px */;
+  line-height: 1.75rem /* 28px */;
+`;
+
+const textXLStyles = css`
+  font-size: 1.25rem /* 20px */;
+  line-height: 1.75rem /* 28px */;
+`;
+
+const text2XLStyles = css`
+  font-size: 1.5rem /* 24px */;
+  line-height: 2rem /* 32px */;
+`;
+
+const text4XLStyles = css`
+  font-size: 2.25rem /* 36px */;
+  line-height: 2.5rem /* 40px */;
+`;
+
+/**
+ * Typography Components
+ */
+
+export const H1 = styled.h1<TypographyProps>`
+  font-weight: "bold";
+  ${text2XLStyles}
+  ${up("md")} {
+    ${text4XLStyles}
+  }
+  ${typography}
+`;
+
+export const H2 = styled.h2<TypographyProps>`
+  font-weight: "bold";
+  ${textXLStyles}
+  ${up("md")} {
+    ${text2XLStyles}
+  }
+  ${typography}
+`;
+
+export const H3 = styled.h3<TypographyProps>`
+  font-weight: "bold";
+  ${textLargeStyles}
+  ${up("md")} {
+    ${textXLStyles}
+  }
+  ${typography}
+`;
+
+export const Paragraph = styled.p<TypographyProps>`
+  ${textSmallStyles}
+  ${up("md")} {
+    ${textBaseStyles}
+  }
+  ${typography}
+`;
+
+export const Typography = {
+  H1,
+  H2,
+  H3,
+  Paragraph,
+};
