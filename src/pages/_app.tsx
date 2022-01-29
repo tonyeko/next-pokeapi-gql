0df59a94f83f93aff90a4d24d5e618736c9cf85c
tonyeko <tony.yuwono@gmail.com>
2022-01-29 22:43:15 +0700

    normalize css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,8 @@ import { TransitionLayout } from "@/components/layouts";
 import { globalStyles } from "@/shared/global";
 import { apolloClient } from "@/lib";
 
+import "@/css/reset.css";
+
 const MyApp = ({ Component, pageProps, router }: AppProps) => (
   <>
     <Global styles={globalStyles} />
